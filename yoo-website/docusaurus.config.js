2b565bc7293cc90b8ffd0ceaf6864565265d0a97
hevinci <hevinci@hotmail.com>
2023-04-08 17:58:03 +0800

    update website

--- yoo-website/docusaurus.config.js
+++ yoo-website/docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gmhevinci/YooAsset-Doc/tree/main/yoo-website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gmhevinci/YooAsset-Doc/tree/main/yoo-website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

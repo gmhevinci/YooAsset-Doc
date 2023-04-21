6a5a9f13dac38620e8b71871e8387632b2e4512e
L <401419353@qq.com>
2023-04-21 10:37:30 +0800

    add search feat

--- yoo-website/docusaurus.config.js
+++ yoo-website/docusaurus.config.js
@@ -94,6 +94,22 @@ const config = {
         additionalLanguages: ['powershell', 'csharp', 'groovy'],
       },
     }),
+    themes: [
+      // ... Your other themes.
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+        ({
+          // ... Your options.
+          // `hashed` is recommended as long-term-cache of index file is possible.
+          hashed: true,
+          // For Docs using Chinese, The `language` is recommended to set to:
+          // ```
+          // language: ["en", "zh"],
+          // ```
+        }),
+      ],
+    ],
 };
 
 module.exports = config;
